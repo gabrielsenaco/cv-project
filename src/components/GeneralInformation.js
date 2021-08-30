2f1dfeacfdf76fba6a96f610f754ee63777bc5ff
Gabriel <gabesenacom@gmail.com>
2021-08-29 23:18:19 -0300

    Clear GeneralInformation to use new object's modules scheme

--- src/components/GeneralInformation.js
+++ src/components/GeneralInformation.js
@@ -1,85 +1,73 @@
 import uniqid from 'uniqid'
-import { IconPlus } from '@tabler/icons'
+import { createParentSectionObject, createSectionObject } from './ObjectBuilder'
+
 import {
-  createItemObject,
-  createButtonObject,
-  createParentSectionObject,
-  createSectionObject,
-  createFailObject,
-  createValidatorObject,
-  createValidatorItemObject
-} from './ObjectBuilder'
+  createItemPhoneNumber,
+  createItemEmail,
+  createItemName
+} from './SectionItemObjects'
+
+import {
+  createSaveButton,
+  createEditButton,
+  createCloseEditorButton
+} from './SectionButtonObjects'
+
+import { nameValidator } from './ValidatorObjects'
 
 export default class GeneralInformation {
-
-  constructor(changeInputHandler, toggleEditorHandler, submitHandler) {
+  constructor (changeInputHandler, toggleEditorHandler, submitHandler) {
     this.changeInputHandler = changeInputHandler
     this.toggleEditorHandler = toggleEditorHandler
     this.sectionID = uniqid()
     this.parentSectionID = uniqid()
     this.items = this.getItemsArray(this.sectionID, this.parentSectionID)
     this.buttons = this.getButtonsArray(this.sectionID, this.parentSectionID)
-    this.section = createSectionObject(null, this.items, this.buttons, true, submitHandler, true, this.parentSectionID, this.sectionID)
+    this.section = createSectionObject(
+      null,
+      this.items,
+      this.buttons,
+      true,
+      submitHandler,
+      true,
+      this.parentSectionID,
+      this.sectionID
+    )
 
-    this.sectionID2 = uniqid()
-    this.items2 = this.getItemsArray(this.sectionID2, this.parentSectionID)
-    this.buttons2 = this.getButtonsArray(this.sectionID2, this.parentSectionID)
-    this.section2 = createSectionObject(null, this.items2, this.buttons2, true, submitHandler, true, this.parentSectionID, this.sectionID2)
-    
-    this.parentSection = createParentSectionObject('General Information', [this.section, this.section2], this.getValidators(), this.section2, this.parentSectionID)
+    this.parentSection = createParentSectionObject(
+      'General Information',
+      [this.section],
+      this.getValidators(),
+      this.section,
+      this.parentSectionID
+    )
   }
 
-  getData() {
+  getData () {
     return this.parentSection
   }
 
-  getItemsArray(sectionID, parentSectionID) {
+  getItemsArray (sectionID, parentSectionID) {
     return [
-      createItemObject(
-        'Name',
-        'text',
-        'Enter your name here',
-        IconPlus,
-        this.changeInputHandler,
+      createItemName(sectionID, parentSectionID, this.changeInputHandler),
+      createItemEmail(sectionID, parentSectionID, this.changeInputHandler),
+      createItemPhoneNumber(sectionID, parentSectionID, this.changeInputHandler)
+    ]
+  }
+
+  getButtonsArray (sectionID, parentSectionID) {
+    return [
+      createSaveButton(sectionID, parentSectionID),
+      createEditButton(sectionID, parentSectionID, this.toggleEditorHandler),
+      createCloseEditorButton(
         sectionID,
-        parentSectionID
-      ),
-      createItemObject(
-        'Email',
-        'email',
-        'Enter your email here',
-        IconPlus,
-        this.changeInputHandler,
-        sectionID,
-        parentSectionID
-      ),
-      createItemObject(
-        'Phone number',
-        'number',
-        'Enter your phone number here',
-        IconPlus,
-        this.changeInputHandler,
-        sectionID,
-        parentSectionID
+        parentSectionID,
+        this.toggleEditorHandler
       )
     ]
   }
 
-  getButtonsArray(sectionID, parentSectionID) {
-    return [
-      createButtonObject('Save', 'submit', IconPlus, 'black', 'success', false, true, null, sectionID, parentSectionID),
-      createButtonObject('Edit', null, IconPlus, 'black', 'success', true, false, this.toggleEditorHandler, sectionID, parentSectionID)
-    ]
+  getValidators () {
+    return [nameValidator]
   }
-
-  getValidators() {
-    return [createValidatorObject('name', 'text', (value) => {
-      if(value.length === 0) {
-       return createValidatorItemObject(false, 'Name cannot be empty')
-      }
-      
-      return createValidatorItemObject(true)
-    })]
-  }
-
 }
