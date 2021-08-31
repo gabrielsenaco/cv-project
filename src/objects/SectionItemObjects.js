cb3b3287b0b466b229a88b2fb243a7ba9a8f62ea
Gabriel <gabesenacom@gmail.com>
2021-08-30 22:51:17 -0300

    Add Section items objects models

--- src/objects/SectionItemObjects.js
+++ src/objects/SectionItemObjects.js
@@ -1,4 +1,4 @@
-import { IconSignature, IconAt, IconPhone } from '@tabler/icons'
+import { IconSignature, IconAt, IconPhone, IconSchool, IconCertificate, IconCalendar, IconBuildingSkyscraper, IconBriefcase, IconSubtask} from '@tabler/icons'
 import { createItemObject } from './ObjectBuilder'
 
 const createItemName = (sectionID, parentSectionID, changeInputHandler) => {
@@ -41,4 +41,100 @@ const createItemPhoneNumber = (
   )
 }
 
-export { createItemPhoneNumber, createItemEmail, createItemName }
+const createItemSchoolName = (sectionID, parentSectionID, changeInputHandler) => {
+  return createItemObject(
+    'School name',
+    'text',
+    'Enter school name here',
+    IconSchool,
+    changeInputHandler,
+    sectionID,
+    parentSectionID
+  )
+}
+
+const createItemTitleOfStudy = (sectionID, parentSectionID, changeInputHandler) => {
+  return createItemObject(
+    'Title of study',
+    'text',
+    'Enter title of study here',
+    IconCertificate,
+    changeInputHandler,
+    sectionID,
+    parentSectionID
+  )
+}
+
+const createItemDateOfStudy= (sectionID, parentSectionID, changeInputHandler) => {
+  return createItemObject(
+    'Date of study',
+    'date',
+    'Enter date of study here',
+    IconCalendar,
+    changeInputHandler,
+    sectionID,
+    parentSectionID
+  )
+}
+
+const createItemCompanyName = (sectionID, parentSectionID, changeInputHandler) => {
+  return createItemObject(
+    'Company name',
+    'text',
+    'Enter company name here',
+    IconBuildingSkyscraper,
+    changeInputHandler,
+    sectionID,
+    parentSectionID
+  )
+}
+
+const createItemPositionTitle = (sectionID, parentSectionID, changeInputHandler) => {
+  return createItemObject(
+    'Position title',
+    'text',
+    'Enter position title here',
+    IconBriefcase,
+    changeInputHandler,
+    sectionID,
+    parentSectionID
+  )
+}
+
+const createItemMainTasks = (sectionID, parentSectionID, changeInputHandler) => {
+  return createItemObject(
+    'Main tasks',
+    'textarea',
+    'Enter your main tasks here',
+    IconSubtask,
+    changeInputHandler,
+    sectionID,
+    parentSectionID
+  )
+}
+
+const createItemDateStartedJob = (sectionID, parentSectionID, changeInputHandler) => {
+  return createItemObject(
+    'Date you started',
+    'date',
+    'Enter the date here',
+    IconCalendar,
+    changeInputHandler,
+    sectionID,
+    parentSectionID
+  )
+}
+
+const createItemDateStoppedJob = (sectionID, parentSectionID, changeInputHandler) => {
+  return createItemObject(
+    'Date you stopped',
+    'date',
+    'Enter the date here',
+    IconCalendar,
+    changeInputHandler,
+    sectionID,
+    parentSectionID
+  )
+}
+
+export { createItemPhoneNumber, createItemEmail, createItemName, createItemDateOfStudy, createItemTitleOfStudy, createItemSchoolName, createItemDateStoppedJob, createItemDateStartedJob, createItemMainTasks, createItemPositionTitle, createItemCompanyName }
