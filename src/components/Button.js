1d338b3a5d0639546150f257434f93e861bad09b
Gabriel <gabesenacom@gmail.com>
2021-09-01 20:08:55 -0300

    Apply Prettier

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import './../styles/Button.css'
 
 export default class Button extends React.Component {
   render () {
     const { text, icon, color, bgColor, type, id, clickHandler } = this.props
-    const className = 'bg-'.concat(bgColor || 'default', ' ', color || '')
+    const className = 'button bg-'.concat(
+      bgColor || 'default',
+      ' ',
+      color || ''
+    )
     return (
       <button
         className={className}
