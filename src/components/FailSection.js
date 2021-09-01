2d35fbca0b65f954e5c6e0f074775b24a81bb955
Gabriel <gabesenacom@gmail.com>
2021-09-01 20:09:27 -0300

    Add Fails section title and fail index

--- src/components/FailSection.js
+++ src/components/FailSection.js
@@ -1,21 +1,28 @@
 import React from 'react'
 import uniqid from 'uniqid'
+import './../styles/FailSection.css'
 
 export default class FailSection extends React.Component {
   render () {
     let fails = this.props.fails || []
-    fails = fails.map(fail => {
+    fails = fails.map((fail, index) => {
       return (
         <li key={uniqid()} className='fail-item'>
+          {<span className='fail-item-index'>{index + 1 + '. '}</span>}
           {fail}
         </li>
       )
     })
 
-    if (this.props.ordered) {
-      return <ol className='fail-section'>{fails}</ol>
-    } else {
-      return <ul className='fail-section'>{fails}</ul>
+    if (fails.length === 0) {
+      return null
     }
+
+    return (
+      <ul className='fail-section'>
+        <h5>Fails list</h5>
+        {fails}
+      </ul>
+    )
   }
 }
